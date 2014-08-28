44e9ccf58dfd57d3728862adba3d76222c257f20
ahwayakchih <ahwayakchih@gmail.com>
2014-08-28 21:02:22 +0200

    Additional explanation comment in test/support/config.js

--- test/support/config.js
+++ test/support/config.js
@@ -1,3 +1,13 @@
+/**
+ * This is a helper for running tests. It will use configuration values passed
+ * through environment variables.
+ *
+ * If `DB_INST` is provided, `DB_PORT` will be ignored.
+ * Tedious will use defaults for some of those, if they're not provided.
+ *
+ * For more information about configuration variables, check
+ * {@link http://pekim.github.io/tedious/api-connection.html#function_newConnection}
+ */
 module.exports = {
 	userName: process.env.DB_USER || false,
 	password: process.env.DB_PASS || false,
